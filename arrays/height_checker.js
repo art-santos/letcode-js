3474f56e2cc9b5e3cfa11c5990b70afcb2b91f32
art-santos <arthursantosdeveloper@gmail.com>
2022-05-27 13:01:22 -0300

    chore: height checker

---
+++ arrays/height_checker.js
@@ -0,0 +1,17 @@
+function main(){
+    console.log(heightChecker([1,1,4,2,1,3]));
+}
+
+
+var heightChecker = function(heights) {
+    const sorted_heights = [...heights].sort((a, b) => a - b);
+    let i = 0;
+    for (let n in heights){
+        heights[n] !== sorted_heights[n]
+        &&
+        i++;
+    }
+    return i;
+};
+
+main();
