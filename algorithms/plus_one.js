1a3cd929b7f6cb7ebb21a0c7b2d5802fc7359ca1
Arthur <arthursantosdeveloper@gmail.com>
2022-12-10 19:05:26 -0300

    chore: plus one

--- algorithms/plus_one.js
+++ algorithms/plus_one.js
@@ -1,8 +1,13 @@
 function main() {
   var plusOne = function (digits) {
-    return Number(arr.join(""));
+    digits = digits.join("");
+    let bigInt = BigInt(digits);
+    bigInt = bigInt + 1n;
+    bigInt = bigInt.toString();
+    bigInt = bigInt.split("");
+    return bigInt.map((item) => Number(item));
   };
-  console.log(plusOne([1, 2, 3]));
+  console.log(plusOne([9]));
 }
 
 main();
