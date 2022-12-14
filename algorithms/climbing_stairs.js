508dae73acab6c97bba81f47573468d3faeb2bb8
Arthur <arthursantosdeveloper@gmail.com>
2022-12-14 04:30:31 -0300

    chore: climbing_stairs

---
+++ algorithms/climbing_stairs.js
@@ -0,0 +1,27 @@
+function main() {
+  var climbStairs = function (n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    if (n >= 3) {
+      let variants = 2;
+      let acc = 0;
+      let acc1 = 1;
+      let acc2 = 2;
+
+      do {
+        acc = acc1 + acc2;
+        if (variants < n) {
+          acc1 = acc2;
+          acc2 = acc;
+        }
+        variants++;
+      } while (variants < n);
+      return acc;
+    }
+  };
+
+  console.log(climbStairs(2));
+}
+
+main();
--- algorithms/sqrt_x.js
+++ algorithms/sqrt_x.js
@@ -1,3 +1,4 @@
 var mySqrt = function (x) {
+  if (x <= 0) return 0;
   return Math.floor(Math.sqrt(x));
 };
